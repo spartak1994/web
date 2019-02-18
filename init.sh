f3211c8c71057d2e437d8579691110677a201870
spartak1994 <spartak1994@mail.ru>
2019-02-18 13:40:25 +0300

    WSGI приложение

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
-sudo gunicorn -c /etc/gunicorn.d/test kek:application
+sudo gunicorn -c hello.py hello:app
 sudo /etc/init.d/gunicorn restart
