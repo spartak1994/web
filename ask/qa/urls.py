1eeaf485111a4cb9d5c3b4157beac9e7e30541f3
spartak1994 <spartak1994@mail.ru>
2019-02-18 16:15:36 +0300

    django first expirience

---
+++ ask/qa/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import url, include
+from django.contrib import admin
+
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.test, name='index'),
+    url(r'^popular/.*$', views.test, name='popular'),    
+    url(r'^ask/.*$', views.test, name='ask'),
+    url(r'^answer/.*$', views.test, name='answer'),
+    url(r'^signup/.*$', views.test, name='signup'),
+    url(r'^login/.*$', views.test, name='login'),
+    url(r'^logout/.*$', views.test, name='logout'),    
+    url(r'^new/.*$', views.test),
+    url(r'^question/(?P<question_id>[0-9]+)/$', views.test, name='question'),    
+]
