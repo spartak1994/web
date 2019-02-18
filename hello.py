eb9f89b15124d5936de8c4b0430f418eb1ac788f
spartak1994 <spartak1994@mail.ru>
2019-02-18 12:55:40 +0300

    WSGI приложение

--- hello.py
+++ hello.py
@@ -1,6 +1,3 @@
 def app(environ, start_response):
-	status = '200 OK'
-	headers = [('Content-Type', 'text/plain')]
-    start_response(status, headers)
-	body = [bytes('\r\n'.join(environ['QUERY_STRING'].split('&')), encoding="utf8")]
-    return body
+    start_response('200 OK', [('Content-Type', 'text/plain')])
+    return [bytes('\r\n'.join(environ['QUERY_STRING'].split('&')), encoding="utf8")]
