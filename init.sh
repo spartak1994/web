f9758a36dcc5710ca93c94023048c1cd20ebce61
spartak1994 <spartak1994@mail.ru>
2019-02-18 16:28:20 +0300

    django first expirience

--- init.sh
+++ init.sh
@@ -1,5 +1,7 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
-sudo gunicorn -c hello.py hello:app
+#sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+#sudo gunicorn -c hello.py hello:app
+
+sudo gunicorn -c /home/box/web/etc/gunicorn-django.conf ask.wsgi:application
 sudo /etc/init.d/gunicorn restart
