29d723cac97ad97d326fc2408604798bc13de888
spartak1994 <spartak1994@mail.ru>
2019-02-19 15:59:06 +0300

    db first expirience

--- init.sh
+++ init.sh
@@ -3,5 +3,8 @@ sudo /etc/init.d/nginx restart
 #sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
 #sudo gunicorn -c hello.py hello:app
 
+sudo /etc/init.d/mysql start
+
 sudo gunicorn -c /home/box/web/etc/django-gunicorn.conf ask.wsgi:application
 sudo /etc/init.d/gunicorn restart
+
