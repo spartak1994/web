1552f37f03a432cc1a36b90f23052fb300e04770
spartak1994 <spartak1994@mail.ru>
2019-02-22 17:10:31 +0300

    db first expirience

--- ask/qa/admin.py
+++ ask/qa/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from qa.models import Question, Answer
+from qa.models import QuestionManager, Question, Answer
 
 # Register your models here.
+admin.site.register(QuestionManager)
 admin.site.register(Question)
 admin.site.register(Answer)
