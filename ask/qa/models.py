57187a350b392995b1c67c2db1cc39b3adcb9272
spartak1994 <spartak1994@mail.ru>
2019-02-22 16:55:22 +0300

    db first expirience

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -15,7 +15,6 @@ class QuestionManager(models.Manager):
     return self.order_by('-rating')	
 	
 class Question(models.Model):
-    objects = QuestionManager()
     title = models.CharField(max_length=255)
     text = models.TextField()
     added_at = models.DateTimeField(blank=True, auto_now_add=True)
@@ -23,6 +22,8 @@ class Question(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     likes = models.ManyToManyField(User, related_name='likes')
 
+	objects = QuestionManager()
+	
     def get_url(self):
         return reverse('question', kwargs={'question_id': self.pk})
     
