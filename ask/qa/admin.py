2aa0cd0ba2f216f635b4eb2a63a610127567a21e
spartak1994 <spartak1994@mail.ru>
2019-02-22 17:26:01 +0300

    db first expirience

--- ask/qa/admin.py
+++ ask/qa/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from qa.models import QuestionManager, Question, Answer
+from qa.models import Question, Answer
 
 # Register your models here.
-admin.site.register(QuestionManager)
 admin.site.register(Question)
 admin.site.register(Answer)
--- ask/qa/models.py
+++ ask/qa/models.py
@@ -9,19 +9,20 @@ from datetime import datetime
 # Create your models here.
 
 class QuestionManager(models.Manager):
-  def new(self):
-    return self.order_by('-added_at')
-  def popular(self):
-    return self.order_by('-rating')	
+	def new(self):
+		return self.order_by('-added_at')
+	def popular(self):
+		return self.order_by('-rating')
 	
 class Question(models.Model):
-    objects = QuestionManager()
     title = models.CharField(max_length=255)
     text = models.TextField()
     added_at = models.DateTimeField(blank=True, auto_now_add=True)
     rating = models.IntegerField(blank=True, null=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     likes = models.ManyToManyField(User, related_name='likes')
+	
+	objects = QuestionManager()
 	
     def get_url(self):
         return reverse('question', kwargs={'question_id': self.pk})
