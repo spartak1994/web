6f0f27357ac02659cbf6c4b7f14ac0626af8a147
spartak1994 <spartak1994@mail.ru>
2019-02-28 17:26:44 +0300

    base views

--- init.sh
+++ init.sh
@@ -14,4 +14,4 @@ sudo /etc/init.d/gunicorn restart
 
 ~/web/ask/manage.py makemigrations
 ~/web/ask/manage.py migrate
-~/web/ask/manage.py runserver 0.0.0.0:8000
+#~/web/ask/manage.py runserver 0.0.0.0:8000
