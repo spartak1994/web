572b5ef8133af939f9be86aed11df7f1c61d3b75
spartak1994 <spartak1994@mail.ru>
2019-03-05 17:32:02 +0300

    base views

--- ask/ask/wsgi.py
+++ ask/ask/wsgi.py
@@ -12,6 +12,6 @@ import sys
 
 from django.core.wsgi import get_wsgi_application
 
-#sys.path.append('/home/box/web/ask')
+sys.path.append('/home/box/web/ask')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ask.settings")
 application = get_wsgi_application()
