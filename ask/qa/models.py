40e253d03a9795b40acecbcce8f87517d847ece2
spartak1994 <spartak1994@mail.ru>
2019-03-12 12:09:43 +0300

    base views

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -12,7 +12,7 @@ class QuestionManager(models.Manager):
         return self.order_by('-rating')
 
 class Question(models.Model):
-    id = models.SlugField(unique=True, primary_key=True)
+    #id = models.SlugField(unique=True, primary_key=True)
     title = models.CharField(max_length=255)
     text = models.TextField()
     added_at = models.DateTimeField(blank=True, auto_now_add=True)
@@ -25,21 +25,20 @@ class Question(models.Model):
         return self.title
 
     def get_url(self):
-        return "/question/{}/".format(self.id)
+        return reverse('question', kwargs={'question_id': self.pk})
     #def get_url(self):
     #    return reverse('question_details', kwargs={'slug': self.id})
 
 
 class Answer(models.Model):
-    id = models.SlugField(unique=True, primary_key=True)
+    #id = models.SlugField(unique=True, primary_key=True)
     text = models.TextField()
     added_at = models.DateTimeField(blank=True, auto_now_add=True)
     question = models.ForeignKey(Question,null=False, on_delete=models.CASCADE)
     author = models.ForeignKey(User,null=True, on_delete=models.SET_NULL)
 
     def __unicode__(self):
-        return "Answer by {0} to question {1}: {2}...".\
-            format(self.author.username, self.question.id, self.text[:50])
+        return "Answer by {0} to question {1}: {2}...".format(self.author.username, self.question.id, self.text[:50])
 	def get_url(self):
         return "/question/{}/".format(self.id)
   
